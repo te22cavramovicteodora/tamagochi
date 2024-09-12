fc8ea3bddbdca7a5436ea0ce3ffe8219fccfcb63
te22cavramovicteodora <teodora.avramovic@elev.ga.ntig.se>
2024-09-12 10:38:57 +0200

    tamagochi

---
+++ tamagochi/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Weapon invandrare = new("invandrare");
+
+invandrare.Attack();
+int damage = Random.Shared.Next(invandrare.MinDamage, invandrare.MaxDamage);
+
+Console.WriteLine(damage);
+Console.ReadLine();
---
+++ tamagochi/tamagochi.cs
@@ -0,0 +1,19 @@
+﻿public class Weapon
+{
+public string Name;
+public int MinDamage;
+public int MaxDamage;
+
+//ange parametrar altsp string name för ett A
+public Weapon(string name)
+{
+    Name = name;
+    MinDamage = Random.Shared.Next(8,12);
+    MaxDamage = Random.Shared.Next(18,25);
+}
+
+public void Attack()
+{
+    Console.WriteLine($"Attacking with {Name}");
+}
+}
